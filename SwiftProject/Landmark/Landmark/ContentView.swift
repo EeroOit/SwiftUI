ff3a39d86d0f25b3caff54634326f63ca3da67a3
Eero <42929818+EeroOit@users.noreply.github.com>
2021-02-01 03:47:12 -0800

    Landmark model initiated

---
+++ SwiftProject/Landmark/Landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by big sur on 1.2.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftProject/Landmark/Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by big sur on 1.2.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
