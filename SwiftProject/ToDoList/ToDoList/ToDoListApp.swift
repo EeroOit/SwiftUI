1ae31af6bdd65c90df4405399b55c96e12d8ee77
Eero <42929818+EeroOit@users.noreply.github.com>
2021-03-26 01:37:05 -0700

    To Do List iniated

---
+++ SwiftProject/ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by big sur on 26.3.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
