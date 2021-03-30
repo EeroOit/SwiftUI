cd77ce0c84ab1e91b6f08c304a55988b981129db
Eero <42929818+EeroOit@users.noreply.github.com>
2021-03-30 01:46:23 -0700

    EnviromentObject implemented

---
+++ SwiftProject/ToDoList/ToDoList/OrderView.swift
@@ -0,0 +1,44 @@
+//
+//  OrderView.swift
+//  ToDoList
+//
+//  Created by big sur on 30.3.2021.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination:
+                                    Text("Check out")) {
+                                    Text("Place order")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+            .listStyle(InsetGroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+            .environmentObject(Order())
+    }
+}
