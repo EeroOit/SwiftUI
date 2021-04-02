c0270aeb628750b2e299fe08765e492a23d6f15e
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-02 04:55:29 -0700

    Swipe to delete and title modifications added

--- SwiftProject/ToDoList/ToDoList/CheckoutView.swift
+++ SwiftProject/ToDoList/ToDoList/CheckoutView.swift
@@ -57,7 +57,9 @@ struct CheckoutView: View {
                 }
                 .pickerStyle(SegmentedPickerStyle())
             }
-            Section(header: Text("TOTAL: \(totalPrice)")) {
+            Section(header: Text("TOTAL: \(totalPrice)")
+                        .font(.largeTitle)
+            ){
                 Button("Confirm order") {
                     showingPaymentAlert.toggle()
                 }
