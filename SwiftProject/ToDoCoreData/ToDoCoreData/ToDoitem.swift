436f14e6685582886205196b36589c0236fc59e9
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-02 05:37:06 -0700

    ToDoItem updated

--- SwiftProject/ToDoCoreData/ToDoCoreData/ToDoitem.swift
+++ SwiftProject/ToDoCoreData/ToDoCoreData/ToDoitem.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import CoreData
+
+public class ToDoItem:NSManagedObject, Identifiable {
+    @NSManaged public var createdAt:Data?
+    @NSManaged public var title:String?
+}
+
+extension ToDoItem {
+    static func getAllToDoItems() -> NSFetchRequest<ToDoItem> {
+        let request:NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest() as!
+        NSFetchRequest<ToDoItem>
+        
+        let SortDescriptor = NSSortDescriptor (key: "createdAt", ascending: true)
+        
+        request.sortDescriptors = [SortDescriptor]
+        
+        return request
+    }
+}
