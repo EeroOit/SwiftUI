37e04ad0e8edaa9bf8bdbd50da1a9e03b1f23d10
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-05 01:38:17 -0700

    Refactoring of the code base

---
+++ SwiftProject/ToDoCoreData/CoreDataToDo/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  CoreDataToDo
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(fetchRequest: ToDoItem.getAllToDoItems()) var toDoItems:FetchedResults<ToDoItem>
+    
+    @State private var newTodoItem = ""
+    
+    var body: some View {
+        NavigationView{
+            List{
+                Section(header: Text("Mitä seuraavaksi")){
+                    HStack{
+                        TextField("Uusi asia", text: self.$newTodoItem)
+                        Button(action: {
+                            let toDoItem = ToDoItem(context: self.managedObjectContext)
+                            toDoItem.title = self.newTodoItem
+                            toDoItem.createdAt = Date()
+                            
+                            do {
+                                try self.managedObjectContext.save()
+                            }catch{
+                                print(error)
+                            }
+                            
+                            self.newTodoItem = ""
+                            
+                        }){
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.green)
+                                .imageScale(.large)
+                        }
+                    }
+                }.font(.headline)
+                Section(header: Text("To Do lista")) {
+                    ForEach(self.toDoItems) {todoItem in
+                        ToDoItemView(title: todoItem.title!, createdAt: "\(todoItem.createdAt!)")
+                    }.onDelete {indexSet in
+                        let deleteItem = self.toDoItems[indexSet.first!]
+                        self.managedObjectContext.delete(deleteItem)
+                        
+                        do {
+                            try self.managedObjectContext.save()
+                        }catch{
+                            print(error)
+                        }
+                    }
+                  
+                }
+            }
+            .navigationBarTitle(Text("Minun lista"))
+            .navigationBarItems(trailing: EditButton())
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
