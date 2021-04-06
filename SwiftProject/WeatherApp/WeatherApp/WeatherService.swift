975721d2d4e38c3cafd64da803b45df908a159b6
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-06 00:54:25 -0700

    Update WeatherService.swift

--- SwiftProject/WeatherApp/WeatherApp/WeatherService.swift
+++ SwiftProject/WeatherApp/WeatherApp/WeatherService.swift
@@ -27,8 +27,11 @@ private let LocationManager = CLLocationManager()
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard error == nil, let data = data else { return }
-            
-        }
+            if let response = try? JSONDecoder().decode(APIResponse.self, from: data) {
+                self.completionHandler?(Weather(response: response))
+                
+            }
+        }.resume()
     }
 }
 
