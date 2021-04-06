e3d107b3d467a5eb81d0855fd9738e95c9e3fb29
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-06 00:25:42 -0700

    loaction manager added

--- SwiftProject/WeatherApp/WeatherApp/WeatherService.swift
+++ SwiftProject/WeatherApp/WeatherApp/WeatherService.swift
@@ -4,14 +4,33 @@
 //
 //  Created by big sur on 5.4.2021.
 //
-
+import CoreLocation
 import Foundation
 
-struct ApiWeather: Decodable {
+public final class WeatherService: NSObject {
+    
+private let LocationManager = CLLocationManager()
+    
+}
+
+struct APIResponse: Decodable {
+    let name: String
+    let main: APIMain
+    let weather: [APIWeather]
+}
+
+struct APIMain: Decodable {
+    let temp: Double
+}
+
+struct APIWeather: Decodable {
     let description: String
     let iconName: String
     
-    enum CodingKeys: String, CodingKeys{
+    enum CodingKeys: String, CodingKey {
+        case description
+        case iconName = "main"
         
     }
 }
+
