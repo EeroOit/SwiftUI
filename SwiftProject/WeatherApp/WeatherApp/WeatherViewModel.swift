63b5af68d7ddbdf4be94102315a520ed8ad866d5
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-06 01:32:25 -0700

    refresh func

--- SwiftProject/WeatherApp/WeatherApp/WeatherViewModel.swift
+++ SwiftProject/WeatherApp/WeatherApp/WeatherViewModel.swift
@@ -30,6 +30,19 @@ public class WeatherViewModel: ObservableObject {
     
     public let weatherService: WeatherService
     
-    public init(weatherService: WeatherService)
+    public init(weatherService: WeatherService) {
+        self.weatherService = weatherService
+    }
+    
+    public func refresh() {
+        weatherService.loadWeatherData { weather in
+            DispatchQueue.main.async {
+                self.cityName = weather.city
+                self.temperature = "\(weather.temperature)°C"
+                self.weatherDescription = weather.description.capitalized
+                self.weatherIcon = iconMap[weather.iconName] ?? defaultIcon
+     }
+    }
+  }
 }
 
