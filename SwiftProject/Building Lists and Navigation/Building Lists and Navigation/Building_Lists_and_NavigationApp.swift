db95d791d1740e9779cbdc5be61f40003864509e
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-06 03:37:22 -0700

    Lists and navigation iniated

---
+++ SwiftProject/Building Lists and Navigation/Building Lists and Navigation/Building_Lists_and_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Building_Lists_and_NavigationApp.swift
+//  Building Lists and Navigation
+//
+//  Created by big sur on 6.4.2021.
+//
+
+import SwiftUI
+
+@main
+struct Building_Lists_and_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
