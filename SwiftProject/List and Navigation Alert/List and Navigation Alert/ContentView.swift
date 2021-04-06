d07beee358067ef55d2d7bec75ec82a3801e90a4
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-06 03:47:04 -0700

    List and navigation iniated

---
+++ SwiftProject/List and Navigation Alert/List and Navigation Alert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  List and Navigation Alert
+//
+//  Created by big sur on 6.4.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
