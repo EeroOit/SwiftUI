4245b725028d3f709d46e1d3169957cbc6a8593b
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-06 01:25:24 -0700

    Using emojies as weather icons

--- SwiftProject/WeatherApp/WeatherApp/WeatherViewModel.swift
+++ SwiftProject/WeatherApp/WeatherApp/WeatherViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+private let defaultIcon = "?"
+private let iconMap = [
+    "Drizzle" : "🌦",
+    "Thunderstrorm" : "⛈",
+    "Rain" : "🌧",
+    "Snow" : "❄️",
+    "Clear" : "☀️",
+    "Clouds" : "☁️"
+
+    
+    
+
+
+
+
+]
+public class WeatherViewModel: ObservableObject {
+    @Published var cityName: String = "City Name"
+    @Published var temperature: String = "--"
+    @Published var weatherDescription: String = "--"
+    @Published var weatherIcon: String = defaultIcon
+    
+    public let weatherService: WeatherService
+    
+    public init(weatherService: WeatherService)
+}
+
