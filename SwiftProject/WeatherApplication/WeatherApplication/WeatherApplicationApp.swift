a408a677c6168d33c2be9de840d6b7d9a7e3a2cf
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-06 02:50:42 -0700

    Rewrite of code due to fix weird error

---
+++ SwiftProject/WeatherApplication/WeatherApplication/WeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationApp.swift
+//  WeatherApplication
+//
+//  Created by big sur on 6.4.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
