e4b19e8c5f528c413b7c15bcdc2115ca3faf69ea
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-09 01:30:14 -0700

    fix typo

--- SwiftProject/Landmarks/Landmarks/Views/MapView.swift
+++ SwiftProject/Landmarks/Landmarks/Views/MapView.swift
@@ -33,8 +33,3 @@ struct MapView_Previews: PreviewProvider {
     }
 }
 
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
-    }
-}
