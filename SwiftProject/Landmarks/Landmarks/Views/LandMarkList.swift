d59a02086f5bc514e98a8c6c9bfc1350367cd6b6
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-09 01:02:56 -0700

    Create LandMarkList.swift

---
+++ SwiftProject/Landmarks/Landmarks/Views/LandMarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandMarkList.swift
+//  Landmarks
+//
+//  Created by big sur on 9.4.2021.
+//
+
+import SwiftUI
+
+struct LandMarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandMarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandMarkList()
+    }
+}
