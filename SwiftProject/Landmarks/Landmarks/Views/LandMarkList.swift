0dea14ac9ca67f1fd2e0f9203ce71a7b222c62b0
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-09 01:31:26 -0700

    Pass the current landmark to the destination LandmarkDetail

--- SwiftProject/Landmarks/Landmarks/Views/LandMarkList.swift
+++ SwiftProject/Landmarks/Landmarks/Views/LandMarkList.swift
@@ -10,15 +10,14 @@ import SwiftUI
 struct LandmarkList: View {
     var body: some View {
         NavigationView {
-        List(landmarks) { landmark in
-            NavigationLink(destination: LandmarkDetail()) {
-                                LandmarkRow(landmark: landmark)
-                            }
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
-        .navigationTitle("Landmarks")
-
     }
-  }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
