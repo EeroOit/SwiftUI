1a82a24c2f2b34b8e12c8a86567db2b7500bacec
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-09 01:01:07 -0700

    Landmark row hstack added

--- SwiftProject/Landmarks/Landmarks/Views/LandMarkRow.swift
+++ SwiftProject/Landmarks/Landmarks/Views/LandMarkRow.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
-struct LandMarkRow: View {
+struct LandmarkRow: View {
+    var landmark: Landmark
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+            Spacer()
+        }
     }
 }
 
-struct LandMarkRow_Previews: PreviewProvider {
+struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandMarkRow()
+        LandmarkRow(landmark: landmarks[0])
     }
 }
