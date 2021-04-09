26ec4733f08a3362709c27cbb61c6b8671a7abcd
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-09 01:18:03 -0700

    Update LandMarkList.swift

--- SwiftProject/Landmarks/Landmarks/Views/LandMarkList.swift
+++ SwiftProject/Landmarks/Landmarks/Views/LandMarkList.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
+        NavigationView {
         List(landmarks) { landmark in
             LandmarkRow(landmark: landmark)
         }
     }
+  }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
