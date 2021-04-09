b71efe0e9473ecabac4b61a1a9b749e6ae52d31a
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-09 01:22:47 -0700

    Update MapView.swift

--- SwiftProject/Landmarks/Landmarks/Views/MapView.swift
+++ SwiftProject/Landmarks/Landmarks/Views/MapView.swift
@@ -10,13 +10,13 @@ import MapKit
 
 struct MapView: View {
     var coordinate: CLLocationCoordinate2D
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    @State private var region = MKCoordinateRegion()
 
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
     }
 
     private func setRegion(_ coordinate: CLLocationCoordinate2D) {
@@ -24,6 +24,12 @@ struct MapView: View {
             center: coordinate,
             span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
         )
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
     }
 }
 
