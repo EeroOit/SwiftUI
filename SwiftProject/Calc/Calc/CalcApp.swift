a365c6e4500258517787e01b04ba582e1a86011f
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-13 08:24:43 -0700

    Calculator project iniated

---
+++ SwiftProject/Calc/Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by big sur on 13.4.2021.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
