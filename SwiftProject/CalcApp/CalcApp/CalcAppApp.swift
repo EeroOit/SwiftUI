5cc5be284686966b40f11ae8cc93c850854fd32a
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-27 03:18:03 -0700

    calc app iniated

---
+++ SwiftProject/CalcApp/CalcApp/CalcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcAppApp.swift
+//  CalcApp
+//
+//  Created by big sur on 27.4.2021.
+//
+
+import SwiftUI
+
+@main
+struct CalcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftProject/CalcApp/CalcApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalcApp
+//
+//  Created by big sur on 27.4.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
