3a92a386ec9e5715c66f3c06e80fa6634d41e3f3
Eero <42929818+EeroOit@users.noreply.github.com>
2021-04-27 03:21:59 -0700

    vstack and colors added

--- SwiftProject/CalcApp/CalcApp/ContentView.swift
+++ SwiftProject/CalcApp/CalcApp/ContentView.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 
+let primaryColor = Color.init(red: 0, green: 116/255, blue: 178/255, opacity: 1.0)
+
 struct ContentView: View {
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+            .background(primaryColor)
+            VStack {
+            }
+            .background(Color.black)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 414, maxHeight: .infinity, alignment: .topLeading)
+        }
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
